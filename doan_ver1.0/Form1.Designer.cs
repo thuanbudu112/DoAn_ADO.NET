12650ee2b97a717dc8d65b957f0ab26d21eebdab
hieppro <lehiep08052005@gmail.com>
2024-11-25 21:34:30 +0700

    edit code

--- doan_ver1.0/Form1.Designer.cs
+++ doan_ver1.0/Form1.Designer.cs
@@ -31,19 +31,19 @@
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.btn_login_user = new System.Windows.Forms.Button();
+            this.txtMk_Nv = new System.Windows.Forms.TextBox();
+            this.txtTenDN_Nv = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btn_login_admin = new System.Windows.Forms.Button();
             this.txt_Pass_admin = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
             this.txt_user_admin = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.txtTenDN_Nv = new System.Windows.Forms.TextBox();
-            this.txtMk_Nv = new System.Windows.Forms.TextBox();
-            this.btnDangNhap_nv = new System.Windows.Forms.Button();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.tabPage2.SuspendLayout();
@@ -54,7 +54,8 @@
             this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(600, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -62,101 +63,159 @@
             // 
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Location = new System.Drawing.Point(9, 10);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(2);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(747, 382);
+            this.tabControl1.Size = new System.Drawing.Size(560, 310);
             this.tabControl1.TabIndex = 1;
             // 
             // tabPage1
             // 
-            this.tabPage1.Controls.Add(this.btnDangNhap_nv);
+            this.tabPage1.Controls.Add(this.btn_login_user);
             this.tabPage1.Controls.Add(this.txtMk_Nv);
             this.tabPage1.Controls.Add(this.txtTenDN_Nv);
             this.tabPage1.Controls.Add(this.label6);
             this.tabPage1.Controls.Add(this.label5);
             this.tabPage1.Controls.Add(this.label1);
-            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(2);
             this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(739, 353);
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(2);
+            this.tabPage1.Size = new System.Drawing.Size(552, 284);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "tabPage1";
             this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // btn_login_user
+            // 
+            this.btn_login_user.Location = new System.Drawing.Point(224, 214);
+            this.btn_login_user.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_login_user.Name = "btn_login_user";
+            this.btn_login_user.Size = new System.Drawing.Size(95, 34);
+            this.btn_login_user.TabIndex = 5;
+            this.btn_login_user.Text = "Đăng nhập";
+            this.btn_login_user.UseVisualStyleBackColor = true;
+            // 
+            // txtMk_Nv
+            // 
+            this.txtMk_Nv.Location = new System.Drawing.Point(170, 135);
+            this.txtMk_Nv.Margin = new System.Windows.Forms.Padding(2);
+            this.txtMk_Nv.Multiline = true;
+            this.txtMk_Nv.Name = "txtMk_Nv";
+            this.txtMk_Nv.PasswordChar = '*';
+            this.txtMk_Nv.Size = new System.Drawing.Size(242, 31);
+            this.txtMk_Nv.TabIndex = 4;
+            // 
+            // txtTenDN_Nv
+            // 
+            this.txtTenDN_Nv.Location = new System.Drawing.Point(170, 69);
+            this.txtTenDN_Nv.Margin = new System.Windows.Forms.Padding(2);
+            this.txtTenDN_Nv.Multiline = true;
+            this.txtTenDN_Nv.Name = "txtTenDN_Nv";
+            this.txtTenDN_Nv.Size = new System.Drawing.Size(242, 31);
+            this.txtTenDN_Nv.TabIndex = 3;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(54, 148);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(112, 18);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Nhập mật khẩu:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(57, 82);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(109, 18);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Tên đăng nhập:";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(162, 17);
+            this.label1.Location = new System.Drawing.Point(122, 14);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(431, 38);
+            this.label1.Size = new System.Drawing.Size(360, 31);
             this.label1.TabIndex = 0;
             this.label1.Text = "ĐĂNG NHẬP: NHÂN VIÊN\r\n";
             // 
             // tabPage2
             // 
-            this.tabPage2.Controls.Add(this.button1);
+            this.tabPage2.Controls.Add(this.btn_login_admin);
             this.tabPage2.Controls.Add(this.txt_Pass_admin);
             this.tabPage2.Controls.Add(this.label4);
             this.tabPage2.Controls.Add(this.txt_user_admin);
             this.tabPage2.Controls.Add(this.label3);
             this.tabPage2.Controls.Add(this.label2);
-            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(2);
             this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(739, 353);
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(2);
+            this.tabPage2.Size = new System.Drawing.Size(552, 284);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "tabPage2";
             this.tabPage2.UseVisualStyleBackColor = true;
-            this.tabPage2.Click += new System.EventHandler(this.tabPage2_Click);
             // 
-            // button1
+            // btn_login_admin
             // 
-            this.button1.Location = new System.Drawing.Point(299, 263);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(127, 42);
-            this.button1.TabIndex = 6;
-            this.button1.Text = "Đăng nhập";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btn_login_admin.Location = new System.Drawing.Point(224, 214);
+            this.btn_login_admin.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_login_admin.Name = "btn_login_admin";
+            this.btn_login_admin.Size = new System.Drawing.Size(95, 34);
+            this.btn_login_admin.TabIndex = 6;
+            this.btn_login_admin.Text = "Đăng nhập";
+            this.btn_login_admin.UseVisualStyleBackColor = true;
+            this.btn_login_admin.Click += new System.EventHandler(this.button1_Click);
             // 
             // txt_Pass_admin
             // 
             this.txt_Pass_admin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txt_Pass_admin.Location = new System.Drawing.Point(226, 166);
+            this.txt_Pass_admin.Location = new System.Drawing.Point(170, 135);
+            this.txt_Pass_admin.Margin = new System.Windows.Forms.Padding(2);
             this.txt_Pass_admin.Multiline = true;
             this.txt_Pass_admin.Name = "txt_Pass_admin";
-            this.txt_Pass_admin.Size = new System.Drawing.Size(321, 37);
+            this.txt_Pass_admin.Size = new System.Drawing.Size(242, 31);
             this.txt_Pass_admin.TabIndex = 5;
-            this.txt_Pass_admin.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(83, 181);
+            this.label4.Location = new System.Drawing.Point(62, 147);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(136, 22);
+            this.label4.Size = new System.Drawing.Size(112, 18);
             this.label4.TabIndex = 4;
             this.label4.Text = "Nhập mật khẩu:";
-            this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
             // txt_user_admin
             // 
             this.txt_user_admin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txt_user_admin.Location = new System.Drawing.Point(226, 85);
+            this.txt_user_admin.Location = new System.Drawing.Point(170, 69);
+            this.txt_user_admin.Margin = new System.Windows.Forms.Padding(2);
             this.txt_user_admin.Multiline = true;
             this.txt_user_admin.Name = "txt_user_admin";
-            this.txt_user_admin.Size = new System.Drawing.Size(321, 37);
+            this.txt_user_admin.Size = new System.Drawing.Size(242, 31);
             this.txt_user_admin.TabIndex = 3;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(83, 100);
+            this.label3.Location = new System.Drawing.Point(62, 81);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(137, 22);
+            this.label3.Size = new System.Drawing.Size(109, 18);
             this.label3.TabIndex = 2;
             this.label3.Text = "Tên đăng nhập:";
             // 
@@ -164,68 +223,25 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(203, 14);
+            this.label2.Location = new System.Drawing.Point(152, 11);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(353, 38);
+            this.label2.Size = new System.Drawing.Size(295, 31);
             this.label2.TabIndex = 1;
             this.label2.Text = "ĐĂNG NHẬP: ADMIN\r\n";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(83, 100);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(137, 22);
-            this.label5.TabIndex = 1;
-            this.label5.Text = "Tên đăng nhập:";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(83, 181);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(136, 22);
-            this.label6.TabIndex = 2;
-            this.label6.Text = "Nhập mật khẩu:";
-            // 
-            // txtTenDN_Nv
-            // 
-            this.txtTenDN_Nv.Location = new System.Drawing.Point(226, 85);
-            this.txtTenDN_Nv.Multiline = true;
-            this.txtTenDN_Nv.Name = "txtTenDN_Nv";
-            this.txtTenDN_Nv.Size = new System.Drawing.Size(321, 37);
-            this.txtTenDN_Nv.TabIndex = 3;
-            // 
-            // txtMk_Nv
-            // 
-            this.txtMk_Nv.Location = new System.Drawing.Point(226, 166);
-            this.txtMk_Nv.Multiline = true;
-            this.txtMk_Nv.Name = "txtMk_Nv";
-            this.txtMk_Nv.Size = new System.Drawing.Size(321, 37);
-            this.txtMk_Nv.TabIndex = 4;
-            // 
-            // btnDangNhap_nv
-            // 
-            this.btnDangNhap_nv.Location = new System.Drawing.Point(299, 263);
-            this.btnDangNhap_nv.Name = "btnDangNhap_nv";
-            this.btnDangNhap_nv.Size = new System.Drawing.Size(127, 42);
-            this.btnDangNhap_nv.TabIndex = 5;
-            this.btnDangNhap_nv.Text = "Đăng nhập";
-            this.btnDangNhap_nv.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
             this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.tabControl1.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
             this.tabPage1.PerformLayout();
@@ -248,8 +264,8 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox txt_Pass_admin;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button btnDangNhap_nv;
+        private System.Windows.Forms.Button btn_login_admin;
+        private System.Windows.Forms.Button btn_login_user;
         private System.Windows.Forms.TextBox txtMk_Nv;
         private System.Windows.Forms.TextBox txtTenDN_Nv;
         private System.Windows.Forms.Label label6;
--- doan_ver1.0/Form1.cs
+++ doan_ver1.0/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,29 +13,77 @@ namespace doan_ver1._0
 {
     public partial class Form1 : Form
     {
+        private home_giaodien home_hienthi;
+        public void set_giaodien()
+        {
+            home_hienthi = new home_giaodien();
+        }
+
+        SqlConnection connect = new SqlConnection("Data Source=MSI\\SQLEXPRESS;Initial Catalog=quanly_cuahang_dienmay;Integrated Security=True;");
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void label2_Click(object sender, EventArgs e)
+        private void result(int i)
+        {
+            if (i == 1)
+            {
+                MessageBox.Show("Đăng nhập thành công");
+                set_giaodien();
+                home_hienthi.ShowDialog();
+            }
+            else
+            {
+                MessageBox.Show("Đăng nhập thất bại hãy kiểm tra lại user, pass", "Warring !!!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void login()
+        {
+            try
+            {
+                connect.Open();
+                SqlCommand login = new SqlCommand("dangnhap_taikhoan", connect);
+                login.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter user = new SqlParameter("@user", txt_user_admin.Text);
+                SqlParameter pass = new SqlParameter("@pass", txt_Pass_admin.Text);
+
+                login.Parameters.Add(user);
+                login.Parameters.Add(pass);
+
+                if ((int)login.ExecuteScalar() > 0)
+                {
+                    result(1);
+                }
+                else
+                {
+                    result(0);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                connect.Close();
+            }
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
         {
 
         }
 
-        private void tabPage2_Click(object sender, EventArgs e)
+
+        private void button1_Click(object sender, EventArgs e)
         {
-
-        }
-
-        private void label4_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-
+            login();
         }
     }
 }
